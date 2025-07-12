a72a9307b3d6075a213e9a794cc41d0e01e1bebd
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-07-12 21:58:53 +0530

    remove unused ReactPlayer component and simplify Reels component

--- src/components/Reels.tsx
+++ src/components/Reels.tsx
@@ -1,29 +1,7 @@
-import ReactPlayer from "react-player";
+import React from "react";
 
 const Reels = () => {
-  return (
-    <>
-      <ReactPlayer
-        src="https://res.cloudinary.com/nikhilmaurya/video/upload/v1752334434/1024X768_3_ro1bru.mp4"
-        playing={true}
-        height="500px"
-        volume={0}
-        style={{
-          borderRadius: "8px",
-          objectFit: "cover",
-        }}
-      />
-      <ReactPlayer
-        src="https://res.cloudinary.com/nikhilmaurya/video/upload/v1752334434/1024X768_3_ro1bru.mp4"
-        playing={true}
-        height="500px"
-        volume={0}
-        style={{
-          borderRadius: "8px",
-          objectFit: "cover",
-        }}
-      />
-    </>
-  );
+  return <div>Reels</div>;
 };
+
 export default Reels;
